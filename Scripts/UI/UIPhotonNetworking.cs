0b54db19594fec1b1d7774e327c8d645e5043f3e
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-03-30 15:38:02 +0700

    Fix missing event deletion

--- Scripts/UI/UIPhotonNetworking.cs
+++ Scripts/UI/UIPhotonNetworking.cs
@@ -63,6 +63,8 @@ public class UIPhotonNetworking : UIBase
     {
         SimplePhotonNetworkManager.onReceivedRoomListUpdate -= OnReceivedRoomListUpdateCallback;
         SimplePhotonNetworkManager.onJoinedLobby -= OnJoinedLobbyCallback;
+        SimplePhotonNetworkManager.onJoinedRoom -= OnJoinedRoomCallback;
+        SimplePhotonNetworkManager.onLeftRoom -= OnLeftRoomCallback;
         SimplePhotonNetworkManager.onDisconnected -= OnDisconnectedCallback;
     }
 
