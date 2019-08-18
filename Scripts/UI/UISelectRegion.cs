9ae9ad8a91afa86061cb2aa1e6d52af830280c7c
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-08-19 01:19:54 +0700

    Add ui select region

---
+++ Scripts/UI/UISelectRegion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISelectRegion : MonoBehaviour
+{
+    public SimplePhotonNetworkManager networkManager;
+    public Dropdown dropdown;
+
+    public void OnSelectRegion(int index)
+    {
+        networkManager.region = dropdown.options[index].text;
+    }
+}
