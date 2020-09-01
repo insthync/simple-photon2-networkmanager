45f0ea8547ca4c336aa05b3febeb6a894223d6de
Insthync <ittipon.bay@gmail.com>
2020-09-01 23:40:29 +0700

    Add join room UI

---
+++ Scripts/UI/UIPhotonJoinRoom.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPhotonJoinRoom : UIBase
+{
+    public InputField roomNameField;
+
+    public void OnClickJoin()
+    {
+        SimplePhotonNetworkManager.Singleton.JoinRoom(roomNameField.text);
+    }
+}
