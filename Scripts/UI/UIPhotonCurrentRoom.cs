3a824a127106b0d17ecdfffc942433c7e7576986
Insthync <ittipon.bay@gmail.com>
2020-09-02 03:15:19 +0700

    Add UI current room, use it to show generic room info

---
+++ Scripts/UI/UIPhotonCurrentRoom.cs
@@ -0,0 +1,20 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPhotonCurrentRoom : MonoBehaviour
+{
+    public Text textName;
+    public Text textPlayerCount;
+
+    private void Update()
+    {
+        var room = PhotonNetwork.CurrentRoom;
+        if (textName != null)
+            textName.text = room.Name;
+        if (textPlayerCount != null)
+            textPlayerCount.text = room.PlayerCount + "/" + room.MaxPlayers;
+    }
+}
