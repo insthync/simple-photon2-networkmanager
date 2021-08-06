e1610a9a07d180da5d4da49dc11b2b306129bd50
Insthync <ittipon.bay@gmail.com>
2021-08-06 07:24:03 +0700

    Delete unused namespace

--- Scripts/Network/ChatterEntity.cs
+++ Scripts/Network/ChatterEntity.cs
@@ -1,10 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
-using Photon;
 using Photon.Pun;
-using Photon.Realtime;
 
 public class ChatterEntity : MonoBehaviourPunCallbacks
 {
